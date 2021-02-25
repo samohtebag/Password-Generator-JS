0fbe136d3977d76506b66631305d7a2a2f368762
gabe thomas <gabe.thomas@bizzabo.com>
2021-02-24 22:46:17 -0500

    added randomization to confirm lines 28 through 52

--- assets/script.js
+++ assets/script.js
@@ -2,11 +2,13 @@
 var generateBtn = document.querySelector("#generate");
 
 // defining variables
-var uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-var lowercase = ["abcdefghijklmnopqrstuvwxyz"];
-var specialcharacters = ["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
-var numbers = ["1234567890"];
-var password = [uppercase, lowercase, specialcharacters, numbers];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialcase = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var password = [uppercase, lowercase, specialcase, numbers];
+
+var passwordFinal="";
 
 // Declaring function getPassLength
 function getPassLength() {
@@ -25,13 +27,38 @@ function getPassLength() {
 
   var confirmUpperCase = confirm("Should the password include uppercase letters?")
   console.log(confirmUpperCase);
+  if (confirmUpperCase) {
+    passwordFinal += uppercase[Math.floor(Math.random()*uppercase.length)];
+  }
+  console.log(passwordFinal);
+
   var confirmLowerCase = confirm("Should the password include lowercase letters?")
   console.log(confirmLowerCase);
+  if (confirmLowerCase) {
+    passwordFinal += lowercase[Math.floor(Math.random()*lowercase.length)];
+  }
+  console.log(passwordFinal);
+
   var confirmspecialcase = confirm("Should the password include special characters?");
-  console.log(confirmspecialcase);
+  if (confirmspecialcase) {
+    passwordFinal += specialcase[Math.floor(Math.random()*specialcase.length)];
+  }
+  console.log(passwordFinal);
+
   var confirmNumbers = confirm ("Should this password include numbers?");
-  console.log(confirmNumbers);
+  if (confirmNumbers) {
+    passwordFinal += numbers[Math.floor(Math.random()*numbers.length)];
+    console.log(passwordFinal);
+  }
+
+  // how many characters should there be in the string
+     
 }
+
+  // make a function to check the password for uppercase
+  // check for lowercase
+  // check for special characters
+  // check numbers 
 
 getPassLength();
 
